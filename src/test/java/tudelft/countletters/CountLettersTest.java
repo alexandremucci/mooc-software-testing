75b688e735d321fd45806e560d7aeb402143c3a0
Alexandre Mucci <email@alexandremucci.com.br>
2023-02-05 08:02:42 -0300

    Course finished.

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,26 +6,21 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
+    public void LatCharS() {
+        int test = new CountLetters().count("car|dog|cats");
+        Assertions.assertEquals(2, test);
+    }
+
+    @Test
+    public void LastCharR() {
+        int words = new CountLetters().count("cats|dog|car");
         Assertions.assertEquals(2, words);
     }
 
     @Test
-    public void lastWordDoesNotMatch() {
+    public void LastCharNotRorS() {
         int words = new CountLetters().count("cats|car|dog");
         Assertions.assertEquals(2, words);
     }
 
-    @Test
-    public void lastCharIsInvalid() {
-        int words = new CountLetters().count("cats|car|dog|");
-        Assertions.assertEquals(2, words);
-    }
-
-    @Test
-    public void lastWordMatches() {
-        int words = new CountLetters().count("cats|car");
-        Assertions.assertEquals(2, words);
-    }
 }
